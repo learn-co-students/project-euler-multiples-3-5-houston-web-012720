97fbf058b6a931bd91c546b263848cc4a6d57a16
placidowang <placidow@gmail.com>
2020-02-12 13:47:47 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,16 +1,19 @@
 # Enter your procedural solution here!
 def collect_multiples(limit)
-    multiples = []
+    # multiples = []
     
-    i = 1
-    while i < limit
-        multiples << i if i % 3 == 0 || i % 5 == 0
-        i += 1
-    end
+    # i = 1
+    # while i < limit
+    #     multiples << i if i % 3 == 0 || i % 5 == 0
+    #     i += 1
+    # end
 
-    multiples
+    # multiples
+
+    Array(1...limit).select{|i| i % 3 == 0 || i % 5 == 0}
 end
 
 def sum_multiples(limit)
-    collect_multiples(limit).reduce(0) {|memo, num| memo + num}
+    # collect_multiples(limit).reduce(0) {|memo, num| memo + num}
+    collect_multiples(limit).sum
 end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -7,18 +7,21 @@ class Multiples
     end
 
     def collect_multiples
-        multiples = []
+        # multiples = []
     
-        i = 1
-        while i < self.limit
-            multiples << i if i % 3 == 0 || i % 5 == 0
-            i += 1
-        end
+        # i = 1
+        # while i < self.limit
+        #     multiples << i if i % 3 == 0 || i % 5 == 0
+        #     i += 1
+        # end
     
-        multiples
+        # multiples
+
+        Array(1...self.limit).select{|i| i % 3 == 0 || i % 5 == 0}
     end
 
     def sum_multiples
-        collect_multiples.reduce(0) {|memo, num| memo + num}
+        # collect_multiples.reduce(0) {|memo, num| memo + num}
+        collect_multiples.sum
     end
 end
