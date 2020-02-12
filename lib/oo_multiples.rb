c9c844ffc822a86162f084d4158958ad96af44d1
GianMoran <gpmoran@uh.edu>
2020-02-12 13:46:06 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -7,7 +7,7 @@ class Multiples
     
     def collect_multiples
 
-        (1...limit).select{|num| num % 3 == 0 || num % 5 == 0}
+        (1...self.limit).select{|num| num % 3 == 0 || num % 5 == 0}
       end
       def sum_multiples
          collect_multiples.sum
