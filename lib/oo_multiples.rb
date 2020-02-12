018459e13c3288350f6027216c4eaeeaf4c348cb
timothyalton <mitchell.alton@live.com>
2020-02-12 13:48:17 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+require 'pry'
+class Multiples
+
+    attr_accessor :limit
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1...@limit).select{|n| n % 3 == 0 || n % 5 == 0}
+    end
+
+    def sum_multiples
+        collect_multiples.sum 
+    end
+end
