898e9cc1db86fa83bc6f2de48e3f7030ccda30e4
DrVonDevious <drvondevious@gmail.com>
2020-02-12 13:35:01 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+
+  arr = *(1..limit-1)
+  arr = arr.select { |n| n%3 == 0 || n%5 == 0 }
+
+  arr 
+
+end 
+
+def sum_multiples(num)
+  collect_multiples(num).sum
+end  
