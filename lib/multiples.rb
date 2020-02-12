fc681507748dc9d0c0ff3249be9e9d6b201833a5
placidowang <placidow@gmail.com>
2020-02-12 13:22:51 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples = []
+    
+    i = 1
+    while i < limit
+        multiples << i if i % 3 == 0 || i % 5 == 0
+        i += 1
+    end
+
+    multiples
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).reduce(0) {|memo, num| memo + num}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_reader :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        multiples = []
+    
+        i = 1
+        while i < self.limit
+            multiples << i if i % 3 == 0 || i % 5 == 0
+            i += 1
+        end
+    
+        multiples
+    end
+
+    def sum_multiples
+        collect_multiples.reduce(0) {|memo, num| memo + num}
+    end
+end
