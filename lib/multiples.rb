a65c1c7df978b774c159b210af29bcff5c204cb5
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-12 13:35:42 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,21 @@
+require_relative 'oo_multiples'
+require 'pry'
 # Enter your procedural solution here!
+
+def collect_multiples(end_num)
+    Multiples.new(end_num).collect_multiples
+    
+    
+    # range = 1...end_num
+    # passing_numbers = []
+    # range.each do |num|
+    #     if num % 3 == 0 || num % 5 == 0
+    #         passing_numbers << num
+    #     end
+    # end
+    # passing_numbers
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
